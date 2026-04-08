ec4123f9373be1adfb5d523a9d3690d456353282
zinininabil-stack <zinini.nabil@gmail.com>
2026-04-08 13:58:56 +0200

    Fix: Update type annotations to use List from typing module

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Module for summing a list of floats.""" 
 
-input_list: list[float]
+from typing import List
+
+input_list: List[float]
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of floats."""
     return float(sum(input_list))
