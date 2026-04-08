deb68f30969542135d2d36ab76e02b9ef9e2fde6
zinininabil-stack <zinini.nabil@gmail.com>
2026-04-08 14:19:41 +0200

    Fix: Update type annotations for element_length function to use Iterable and Sequence

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Module for pairing elements with their lengths."""
 
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
-lst: List[str]
+lst: Iterable[Sequence]
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples ``(element, length of element)``."""
     return [(i, len(i)) for i in lst]
