1ad6fdbda3b3278e46c3f2c2ff34565e653053a2
zinininabil-stack <zinini.nabil@gmail.com>
2026-05-05 10:45:01 +0200

    Add Car class to manage car details with brand, motor, and color

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,27 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
