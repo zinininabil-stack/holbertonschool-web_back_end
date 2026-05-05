3e7fcb5c70929b47712f4813d4b87fb7cf7ceea2
zinininabil-stack <zinini.nabil@gmail.com>
2026-05-05 10:09:03 +0200

    Add Pricing class to manage pricing details with currency validation

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency instance');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
