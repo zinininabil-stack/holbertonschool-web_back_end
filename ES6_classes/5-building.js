b4702f8089f6f2c3b997e64f7a9dd31a08b708c7
zinininabil-stack <zinini.nabil@gmail.com>
2026-05-05 10:17:17 +0200

    Add Building class to manage building details with sqft validation

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    /* If this is a subclass, ensure it implements evacuationWarningMessage */
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
