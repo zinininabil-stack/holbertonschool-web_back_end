a618c6a06dc36014ee194b2d6858a10a67160324
zinininabil-stack <zinini.nabil@gmail.com>
2026-05-07 10:05:43 +0200

    Add initial implementation of groceriesList function to create a grocery Map

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const groceryList = new Map();
+  groceryList.set('Apples', 10);
+  groceryList.set('Tomatoes', 10);
+  groceryList.set('Pasta', 1);
+  groceryList.set('Rice', 1);
+  groceryList.set('Banana', 5);
+  return groceryList;
+}
